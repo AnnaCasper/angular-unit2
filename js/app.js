760edabe33f9f3808a5e13dfbe1491e999180815
Anna Casper <anna.casper3@gmail.com>
2015-09-08 19:12:57 -0600

    attempting

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
-var app = angular.module('redditClone', ['angularMoment', 'ngAnimate', 'ngRoute']);
+var app = angular.module('calculator', ['ngRoute']);
 
-app.config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'partials/home.html',
-        controller: 'HomeController'
-      })
-      .when('/dogs', {
-        templateUrl: 'partials/dogs.html',
-        controller: 'DogsController'
-      })
-      .otherwise({redirectTo: '/', templateUrl: 'partials/home.html'})
-});
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/add/:firstNum/:secondNum', {
+      templateUrl: 'partials/result.html',
+      controller: 'AddCtrl'
+    })
+    .when('/subtract/:firstNum/:secondNum', {
+      templateUrl: 'partials/result.html',
+      controller: 'SubtractCtrl'
+    })
+    .when('/multiply/:firstNum/:secondNum', {
+      templateUrl: 'partials/result.html',
+      controller: 'MultiplyCtrl'
+    })
+    .when('/divide/:firstNum/:secondNum', {
+      templateUrl: 'partials/result.html',
+      controller: 'DivideCtrl'
+    })
+    .otherwise({redirectTo: '/'});
+})
